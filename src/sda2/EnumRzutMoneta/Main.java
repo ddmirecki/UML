f3de5e9409a4936ba6b04a097fe083318e3e51dd
Daniel <ddmirecki@gmail.com>
2017-08-28 14:46:10 +0200

    rzutMoneta1

--- src/sda2/EnumRzutMoneta/Main.java
+++ src/sda2/EnumRzutMoneta/Main.java
@@ -1,5 +1,7 @@
 package sda2.EnumRzutMoneta;
 
+import java.util.Scanner;
+
 /**
  * Created by Daniel on 2017-08-28.
  */
@@ -7,11 +9,20 @@ public class Main {
     public static void main(String[] args) {
 
         Rzut rzut = new Rzut();
-
-        Moneta monetaKomputera = rzut.rzutLosowy();
-        Moneta monetaGracza = rzut.zapytajGracza();
-        String porownanie = rzut.porownanie(monetaKomputera, monetaGracza);
-        System.out.println(porownanie);
-
+        boolean runda = true;
+        while(runda) {
+            String czyJeszczeRaz = "Rzucasz jeszcze raz? T/N";
+            Moneta monetaKomputera = rzut.rzutLosowy();
+            Moneta monetaGracza = rzut.zapytajGracza();
+            String porownanie = rzut.porownanie(monetaKomputera, monetaGracza);
+            System.out.println(porownanie);
+            System.out.println(czyJeszczeRaz);
+            String scanner = new Scanner(System.in).nextLine().toLowerCase();
+            if(scanner.equals("t")){
+                runda = true;
+            }else{
+                runda = false;
+            }
+        }
     }
 }
